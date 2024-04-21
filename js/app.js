486e8ecf5ec6e6b4e15fe47c6e37deee037823d2
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2024-04-21 23:09:02 +0200

    updated

--- js/app.js
+++ js/app.js
@@ -16,4 +16,23 @@ fetch('https://api.waifu.im/search').then(res => res.json()).then(data => {
     imgs.querySelector('.right-img').appendChild(imgElement);
 });
 
-console.log('uwu~~%c', 'font-size: 4rem');
+document.querySelector('.btn#left').addEventListener('click', () => {
+    fetch('https://api.waifu.im/search').then(res => res.json()).then(data => {
+        const img = data.images[0].url;
+        const imgElement = document.createElement('img');
+        imgElement.src = img;
+        imgs.querySelector('.left-img').innerHTML = '';
+        imgs.querySelector('.left-img').appendChild(imgElement);
+    });
+});
+document.querySelector('.btn#right').addEventListener('click', () => {
+    fetch('https://api.waifu.im/search').then(res => res.json()).then(data => {
+        const img = data.images[0].url;
+        const imgElement = document.createElement('img');
+        imgElement.src = img;
+        imgs.querySelector('.right-img').innerHTML = '';
+        imgs.querySelector('.right-img').appendChild(imgElement);
+    });
+});
+
+console.log('%cuwu~~', 'font-size: 4rem');
